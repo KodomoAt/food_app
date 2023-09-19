8aa538e644989be723cdd479efce0e0841a2a9a7
Kodomo At <guillaume@staub.pro>
2023-09-19 23:31:30 +0200

    Add MealItem.js component to render list of each item just style no logic

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,7 @@
 import styles from './AvailableMeals.module.css'
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
 const DUMMY_MEALS = [
     {
         id: 'm1',
@@ -25,13 +28,18 @@ const DUMMY_MEALS = [
         price: 18.99,
     },
 ];
-const AvailableMeals = (props) => {
-    const mealsList = DUMMY_MEALS.map(meal=><li>{meal.name}</li>)
-  return <section className={styles.meals}>
-      <ul>
-          {mealsList}
-      </ul>
-  </section>
+const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id}
+                                                        name={meal.name}
+                                                        description={meal.description}
+                                                        price={meal.price}/>)
+    return <section className={styles.meals}>
+        <Card>
+            <ul>
+                {mealsList}
+            </ul>
+        </Card>
+    </section>
 };
 
 export default AvailableMeals;
