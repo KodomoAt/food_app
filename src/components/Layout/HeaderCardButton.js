0622f91699c4f01ceb060c6721cb7944f91d06ec
Kodomo At <guillaume@staub.pro>
2023-09-20 11:48:38 +0200

    Added an effect that allows the cart button to be animated each time an item is added

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,15 +1,34 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import styles from './HeaderCardButton.module.css'
+import cart from "../Cart/Cart";
 
 const HeaderCardButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const {items} = cartCtx;
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount
     }, 0);
 
-    return <button onClick={props.onClick} className={styles.button}>
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items]);
+
+    return <button onClick={props.onClick} className={btnClasses}>
       <span className={styles.icon}>
 <CartIcon/>
       </span>
